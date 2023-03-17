f53fa5df8512e8ec680db07e400b38be086451b6
JamiuKolawole04 <Jakko4real@gmail.com>
2023-03-17 15:03:00 +0100

    chore: creating and joining room real time connection

--- api/src/room/index.ts
+++ api/src/room/index.ts
@@ -4,14 +4,14 @@ import { v4 as uuidV4 } from "uuid";
 export const roomHandler = (socket: Socket) => {
   const createRoom = () => {
     const roomId: string = uuidV4();
-    socket.join(roomId);
     socket.emit("room-created", { roomId });
 
     console.log("user create a room");
   };
 
-  const joinRoom = () => {
-    console.log("a user joined room");
+  const joinRoom = ({ roomId }: { roomId: string }) => {
+    console.log("a user joined room", roomId);
+    socket.join(roomId);
   };
 
   socket.on("join-room", joinRoom);
--- client/src/pages/room.tsx
+++ client/src/pages/room.tsx
@@ -1,6 +1,15 @@
+import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
+
+import { RoomContext } from "../context/roomContext";
 
 export const Room = () => {
   const { id } = useParams();
+  const { webSocket } = useContext(RoomContext);
+
+  useEffect(() => {
+    webSocket.emit("join-room", { roomId: id });
+  }, []);
+
   return <div>Room {id}</div>;
 };
